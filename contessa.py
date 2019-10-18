53950cb88a5bb960da89e638e9a8c7bfcf998d80
EvilLogic <evillogic1@gmail.com>
2019-10-17 20:32:51 -0700

    works

--- contessa.py
+++ contessa.py
@@ -3,8 +3,16 @@ import discord
 from discord.ext import commands
 
 config = ConfigParser()
+config.read('config.ini')
 
 bot = commands.Bot(command_prefix='&')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 @bot.command()
 async def ping(ctx):
